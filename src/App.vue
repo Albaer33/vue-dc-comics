fda6c584463193f4de0572fadaac6add6f096e7e
Albaer33 <alisi3396@gmail.com>
2021-12-20 15:53:27 +0100

    header done

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,40 @@
 <template>
   <div id="app">
+    <Header/>
+
+    <main>
+      <MainTop/>
+      <MainBottom/>
+    </main>
+
+    <footer>
+      <FooterMenu/>
+      <FooterContact/>
+    </footer>
   </div>
 </template>
 
 <script>
+import Header from "./components/Header.vue";
+import MainTop from "./components/MainTop.vue";
+import MainBottom from "./components/MainBottom.vue";
+import FooterMenu from "./components/FooterMenu.vue";
+import FooterContact from "./components/FooterContact.vue";
 
 export default {
   name: 'App',
   components: {
+    Header,
+    MainTop,
+    MainBottom,
+    FooterMenu,
+    FooterContact,
   }
 }
 </script>
 
 <style lang="scss">
+@import './style/general.scss';
+@import './style/common.scss';
+@import './style/variables.scss';
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,106 @@
+<template>
+    <header class="container">
+        <div class="logo">
+            <img src="../assets/img/dc-logo.png" alt="logo dc">
+        </div>
+        <ul>
+            <li class="item" v-for="(link, index) in links" :key="index">
+                <a class="menu-btn" :href="link.url">{{link.text}}</a>
+            </li>
+        </ul>
+    </header>
+</template>
+
+<script>
+    export default {
+    name: "Header",
+    data: function() {
+        return {
+            links: [
+                {
+                    text: 'CHARACTERS',
+                    url: '#',
+                    current: false
+                },
+                {
+                    text: 'COMICS',
+                    url: '#',
+                    current: false
+                },
+                {
+                    text: 'MOVIES',
+                    url: '#',
+                    current: false
+                },
+                {
+                    text: 'TV',
+                    url: '#',
+                    current: false
+                },
+                {
+                    text: 'GAMES',
+                    url: '#',
+                    current: false
+                },
+                {
+                    text: 'COLLECTIBLES',
+                    url: '#',
+                    current: false
+                },
+                {
+                    text: 'VIDEOS',
+                    url: '#',
+                    current: false
+                },
+                {
+                    text: 'FANS',
+                    url: '#',
+                    current: false
+                },
+                {
+                    text: 'NEWS',
+                    url: '#',
+                    current: false
+                },
+                {
+                    text: 'SHOP',
+                    url: '#',
+                    current: false
+                },
+            ],
+        };
+    }
+}
+</script>
+
+<style scoped lang="scss">
+@import '../style/variables.scss';
+
+header {
+    display: flex;
+    justify-content: space-between;
+    height: 120px;
+    line-height: 120px;
+    ul {
+        list-style-type: none;
+        display: flex;
+    }
+}
+.menu-btn {
+    display: block;
+    margin: 0 10px;
+    color: black;
+    text-decoration: none;
+    &:hover {
+        border-bottom: 4px solid $brand_primary_color;
+    }
+}
+.logo {
+    display: flex;
+    align-items: center;
+    height: 100%;
+    img {
+        height: 80px;
+    }
+}
+</style>
