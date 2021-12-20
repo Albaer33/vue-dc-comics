16def4f4659ad50bbb570a502d3a1b5f5f545789
Albaer33 <alisi3396@gmail.com>
2021-12-20 19:36:05 +0100

    end footer menu

---
+++ src/components/FooterContact.vue
@@ -0,0 +1,24 @@
+<template>
+    <footer class="bg-grey">
+        <div class="footer-container container">
+            <a href="#">SIGN-UP NOW!</a>
+            <div class="social">
+                <div class="text">FOLLOW US</div>
+                <div class="icon"><i class="fab fa-facebook-square"></i></div>
+                <div class="icon"><i class="fab fa-twitter"></i></div>
+                <div class="icon"><i class="fab fa-youtube"></i></div>
+                <div class="icon"><i class="fab fa-pinterest"></i></div>
+                <div class="icon"><i class="fa fa-map-marker" aria-hidden="true"></i></div>
+            </div>
+        </div>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: "FooterContact"
+}
+</script>
+
+<style scoped lang="scss">
+</style>
--- src/components/FooterMenu.vue
+++ src/components/FooterMenu.vue
@@ -1,14 +1,15 @@
 <template>
     <section class="bg-footer">
-        <div class="container">
-            <div v-for="(menu,index) in footerMenus" :key="index">
-                {{menu.title}}
+        <div class="container menu">
+            <div class="single-menu" v-for="(menu,index) in footerMenus" :key="index">
+                <span class="title">{{menu.title}}</span>
                 <ul>
                     <li v-for="(item, j) in menu.items" :key="j">
-                        <a href="#">{{item}}</a>
+                        <a class="link" href="#">{{item}}</a>
                     </li>
                 </ul>
             </div>
+            <img class="bg-dc-logo" src="../assets/img/dc-logo-bg.png" alt="">
         </div>
     </section>
 </template>
@@ -71,4 +72,38 @@ export default {
 </script>
 
 <style scoped lang="scss">
+.bg-footer {
+    position: relative;
+    overflow: hidden;
+    height: 350px;
+    display: flex;
+    background-image: url(../assets/img/footer-bg.jpg);
+}
+.menu {
+    position: relative;
+    padding: 30px 0;
+    display: flex;
+    justify-content: flex-start;
+    .single-menu {
+        margin-right: 30px;
+    }
+}
+.link {
+    font-size: 14px;
+    text-decoration: none;
+    color: grey;
+}
+.title {
+    display: block;
+    color: white;
+    font-size: 20px;
+    font-weight: bold;
+    padding-bottom: 10px;
+}
+.bg-dc-logo {
+    position: absolute;
+    width: 400px;
+    right: 50px;
+    top: -50px
+}
 </style>
